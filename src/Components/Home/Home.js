12e45b6bd111bbc7cfa45a29b97ce68ca35e32b3
Vibhav Salelkar <salelkarvibhav@gmail.com>
2022-01-19 20:58:15 +0530

    feat: home page redirect to chats if user logged in

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,22 @@
 import { Container } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Auth from "../Auth/Auth";
 
 const Home = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const userProfile = JSON.parse(localStorage.getItem("profile"));
+
+    if (userProfile) {
+      history.push("/chats");
+    }
+  }, [history]);
+
   return (
     <Container maxW="xl" centerContent>
-      <Auth/>
+      <Auth />
     </Container>
   );
 };
